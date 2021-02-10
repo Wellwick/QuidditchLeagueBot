e45913c559590861dfb99e8fb3870dd189be5aa9
Michael Boyle <mbbexley@hotmail.co.uk>
2021-02-10 12:58:31 +0000

    Don't be dumb with text manipulation

--- ficparser.py
+++ ficparser.py
@@ -109,7 +109,7 @@ class FicParser(commands.Cog):
         info["round"] = chapter.text[pos:].split("\n")[0]
         if info["round"] == "Unknown" or len(info["round"]) > 50:
             pos = r_text.find("prophet challenge")
-            info["round"] = chapter.text["pos:"].split("\n")[0]
+            info["round"] = chapter.text[pos:].split("\n")[0]
 
         # The vast majority of people go
         # Word Count: nnnn
